658eaad727bee81730a7f1041da830575df058da
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-12-10 18:33:49 +0100

    chore: update vite config to support path alias and add new plugins

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
+import { PrimeVueResolver } from "@primevue/auto-import-resolver";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [vue()],
+  optimizeDeps: {
+    noDiscovery: true,
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 });
