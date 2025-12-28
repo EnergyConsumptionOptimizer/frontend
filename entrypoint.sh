d8c0313a19bf8ee208ac258596943f187af8e39f
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-12-28 12:04:53 +0100

    feat: update Nginx configuration to include threshold service

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-
-
 envsubst '\
-    \${USER_SERVICE_NAME} \${USER_SERVICE_PORT}'< /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
+    \${USER_SERVICE_NAME} \${USER_SERVICE_PORT}\
+    \${THRESHOLD_SERVICE_NAME} \${THRESHOLD_SERVICE_PORT}\'< /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
 exec "$@"
