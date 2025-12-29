6c255b2153011bdb835fdd51fe02cc88ac4536de
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-12-29 15:06:46 +0100

    feat: add forecast service configuration to Nginx and entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 envsubst '\
     \${USER_SERVICE_NAME} \${USER_SERVICE_PORT}\
-    \${THRESHOLD_SERVICE_NAME} \${THRESHOLD_SERVICE_PORT}\'< /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
+    \${THRESHOLD_SERVICE_NAME} \${THRESHOLD_SERVICE_PORT}\
+    \${FORECAST_SERVICE_NAME} \${FORECAST_SERVICE_PORT}\'< /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start Nginx
 exec "$@"
