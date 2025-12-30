324b52e6725c7f49522c390ad7b7cc5372489669
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-30 23:43:14 +0100

    feat: add onboarding step layout
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/layout/OnboardingStepLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  subtitle: String
+})
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center w-full space-y-2">
+    <div class="font-semibold text-xl mb-4">{{ title }}</div>
+    <p>{{ subtitle }}</p>
+  </div>
+
+  <div class="flex justify-center flex-1 mt-4">
+    <slot name="content" />
+  </div>
+</template>
+
+<style>
+
+</style>
