cc7355ced5f72a5b7e5810756836f25989125b9a
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-30 22:28:09 +0100

    feat: add onboarding steps config file
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/config/onboardingSteps.js
@@ -0,0 +1,37 @@
+export const ONBOARDING_STEPS = [
+  {
+    index: 1,
+    path: "upload-floor-plan",
+    name: "onboarding-step-1",
+    label: "Upload floor plan",
+    component: "UploadFloorPlan",
+  },
+  {
+    index: 2,
+    path: "create-zones",
+    name: "onboarding-step-2",
+    label: "Create zones",
+    component: "CreateZones",
+  },
+  {
+    index: 3,
+    path: "add-hookups",
+    name: "onboarding-step-3",
+    label: "Configure hookups",
+    component: "ConfigureSmartFurnitureHookups",
+  },
+  {
+    index: 4,
+    path: "create-household-users",
+    name: "onboarding-step-4",
+    label: "Create users",
+    component: "CreateHouseholdUsers",
+  },
+  {
+    index: 5,
+    path: "configure-thresholds",
+    name: "onboarding-step-5",
+    label: "Configure thresholds",
+    component: "ConfigureThresholds",
+  },
+];
