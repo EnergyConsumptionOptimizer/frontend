e73e818fce20f6eea495f725a3489d497a17281e
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-30 22:17:15 +0100

    feat: add persistedstate plugin for store persistence
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
 import App from "./App.vue";
 import router from "./router";
 
@@ -13,7 +15,10 @@ import "@/assets/styles/tailwind.css";
 
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(router);
 
 app.use(PrimeVue, {
