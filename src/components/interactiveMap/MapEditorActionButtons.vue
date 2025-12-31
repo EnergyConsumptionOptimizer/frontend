862cf093fb06d10feaf2b8806029497a78e1edcd
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-31 01:38:34 +0100

    feat: add Action buttons component to manage elements on the interactive map
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/components/interactiveMap/MapEditorActionButtons.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  isViewMode: Boolean,
+  isDrawMode: Boolean,
+  isEditMode: Boolean,
+})
+</script>
+
+<template>
+  <div class="flex justify-start border rounded-lg p-4 w-full space-x-3">
+    <template v-if="isViewMode">
+      <slot name="viewActions" />
+    </template>
+
+    <template v-if="isDrawMode">
+      <slot name="drawActions" />
+    </template>
+
+    <template v-if="isEditMode">
+      <slot name="editActions" />
+    </template>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
