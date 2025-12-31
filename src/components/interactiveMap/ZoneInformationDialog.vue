49391b3d00c29f83aa3c70fb3b499fea294315ec
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-31 17:54:23 +0100

    fix: use zone model instead on props
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/components/interactiveMap/ZoneInformationDialog.vue
+++ src/components/interactiveMap/ZoneInformationDialog.vue
@@ -1,29 +1,29 @@
 <script setup>
 import Dialog from "primevue/dialog";
-import {computed} from "vue";
+import { computed } from "vue";
 
 const props = defineProps({
-  isOnDrawMode: Boolean
-})
+  isOnDrawMode: Boolean,
+});
 
-const zone = defineModel('zone', { type: Object });
-const visible = defineModel('visible', { type: Boolean, default: false });
-const colorInput = defineModel('colorInput', { type: String });
+const zone = defineModel("zone", { type: Object });
+const visible = defineModel("visible", { type: Boolean, default: false });
+const colorInput = defineModel("colorInput", { type: String });
 
-const emit = defineEmits(['hide', 'save', 'cancel'])
+const emit = defineEmits(["hide", "save", "cancel"]);
 
 const dialogTitle = computed(() =>
-    props.isOnDrawMode ? 'Create new zone' : 'Edit zone'
-)
+  props.isOnDrawMode ? "Create new zone" : "Edit zone",
+);
 </script>
 
 <template>
   <Dialog
-      v-model:visible="visible"
-      modal
-      :header="dialogTitle"
-      :style="{ width: '25rem' }"
-      @hide="emit('hide')"
+    v-model:visible="visible"
+    modal
+    :header="dialogTitle"
+    :style="{ width: '25rem' }"
+    @hide="emit('hide')"
   >
     <span class="text-surface-500 dark:text-surface-400 block mb-8">
       Zone Information
@@ -33,13 +33,18 @@ const dialogTitle = computed(() =>
       <label for="zoneName" class="font-semibold">Name</label>
       <div class="flex flex-col gap-1 w-full">
         <InputText
-            id="zoneName"
-            class="flex-auto"
-            autocomplete="off"
-            v-model="props.zone.name"
-            :invalid="!props.zone.name"
+          id="zoneName"
+          class="flex-auto"
+          autocomplete="off"
+          v-model="zone.name"
+          :invalid="!zone.name"
         />
-        <Message v-show="!props.zone.name" severity="error" variant="simple" size="small">Zone name is required
+        <Message
+          v-show="!zone.name"
+          severity="error"
+          variant="simple"
+          size="small"
+          >Zone name is required
         </Message>
       </div>
     </div>
@@ -47,30 +52,24 @@ const dialogTitle = computed(() =>
     <div class="flex items-center gap-4 mb-4">
       <label for="zoneColor" class="font-semibold">Color</label>
       <ColorPicker
-          inputId="zoneColor"
-          v-model="colorInput"
-          format="hex"
-          pt:root:class="flex-1 flex !w-full"
-          pt:preview:class="flex-1 !h-8 !w-full"
+        inputId="zoneColor"
+        v-model="colorInput"
+        format="hex"
+        pt:root:class="flex-1 flex !w-full"
+        pt:preview:class="flex-1 !h-8 !w-full"
       />
     </div>
 
     <div class="flex justify-end gap-2">
       <Button
-          type="button"
-          label="Cancel"
-          severity="secondary"
-          @click="emit('cancel')"
+        type="button"
+        label="Cancel"
+        severity="secondary"
+        @click="emit('cancel')"
       />
-      <Button
-          type="button"
-          label="Save"
-          @click="emit('save')"
-      />
+      <Button type="button" label="Save" @click="emit('save')" />
     </div>
   </Dialog>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
