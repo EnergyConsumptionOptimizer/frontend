942ccccfa312571985b7fbd2fb9947e76488b35a
rares vasiliu <rv.vasiliu@gmail.com>
2025-12-31 16:01:20 +0100

    feat: add composable to drag smart furniture hookups in the map
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/composables/interactiveMap/useSmartFurnitureHookupDrag.js
@@ -0,0 +1,57 @@
+import { ref } from "vue";
+import { useSmartFurnitureHookupZoneDetection } from "@/composables/interactiveMap/useSmartFurnitureHookupZoneDetection.js";
+
+export function useSmartFurnitureHookupDrag(existingZones) {
+  const { findZoneForSmartFurnitureHookup } =
+    useSmartFurnitureHookupZoneDetection(existingZones);
+
+  const dragState = ref({
+    isDragging: false,
+    hookup: null,
+    startPosition: null,
+  });
+
+  function startDragSmartFurnitureHookup(hookup, position) {
+    dragState.value = {
+      isDragging: true,
+      hookup: hookup,
+      startPosition: { ...position },
+    };
+  }
+
+  function handleDragMove(currentPosition) {
+    if (!dragState.value.isDragging || !dragState.value.startPosition) {
+      return;
+    }
+
+    const dx = currentPosition.x - dragState.value.startPosition.x;
+    const dy = currentPosition.y - dragState.value.startPosition.y;
+
+    dragState.value.hookup.position.x += dx;
+    dragState.value.hookup.position.y += dy;
+
+    dragState.value.startPosition = { ...currentPosition };
+  }
+
+  function stopDrag() {
+    if (!dragState.value.isDragging) return;
+
+    if (dragState.value.hookup.id) {
+      dragState.value.hookup.zone = findZoneForSmartFurnitureHookup(
+        dragState.value.hookup,
+      );
+    }
+
+    dragState.value = {
+      isDragging: false,
+      hookup: null,
+      startPosition: null,
+    };
+  }
+
+  return {
+    startDragSmartFurnitureHookup,
+    handleDragMove,
+    stopDrag,
+  };
+}
