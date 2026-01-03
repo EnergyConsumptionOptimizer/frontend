4cce1fa3941dd72859eab190ad443e0fdecf33cf
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2026-01-03 17:48:45 +0100

    fix: add PrimeVue Badge and Tooltip directives and components to main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import router from "./router";
 import PrimeVue from "primevue/config";
 import ConfirmationService from "primevue/confirmationservice";
 import ToastService from "primevue/toastservice";
+import BadgeDirective from "primevue/badgedirective";
+import Tooltip from "primevue/tooltip";
+import Badge from "primevue/badge";
+import OverlayBadge from "primevue/overlaybadge";
 import EcoTheme from "@/assets/styles/ecoTheme.js";
 
 import "@/assets/styles/styles.scss";
@@ -32,5 +36,9 @@ app.use(PrimeVue, {
 
 app.use(ToastService);
 app.use(ConfirmationService);
+app.directive("badge", BadgeDirective);
+app.directive("tooltip", Tooltip);
+app.component("Badge", Badge);
+app.component("OverlayBadge", OverlayBadge);
 
 app.mount("#app");
