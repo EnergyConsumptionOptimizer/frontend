ac91caac721221fd7ffc9e0fe81630391bec7c24
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-10 10:59:26 +0100

    feat: add methods to save the house map elements in MapService
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/service/MapService.js
+++ src/service/MapService.js
@@ -1,15 +1,48 @@
 import apiClient from "@/middlewares/authInterceptor";
+import { SmartFurnitureHookupService } from "@/service/SmartFurnitureService.js";
 
 const BASE_URL = "/house-map";
 
 export const MapService = {
   async getFloorPlan() {
-    console.log("!!!");
     try {
       const { data } = await apiClient.get(`${BASE_URL}/floor-plan`);
       return data.svgContent || null;
     } catch {
       return null;
     }
+  },
+  async saveFloorPlan(svgData) {
+    const { data } = await apiClient.post(`${BASE_URL}/floor-plan`, {
+      svgContent: svgData,
+    });
+
+    return data;
+  },
+
+  async saveZone(zone) {
+    const { data } = await apiClient.post(`${BASE_URL}/zones`, {
+      name: zone.name,
+      color: zone.color,
+      vertices: zone.points,
+    });
+    return data;
+  },
+
+  async saveSmartFurnitureHookup(smartFurnitureHookup) {
+    const result =
+      await SmartFurnitureHookupService.saveSmartFurnitureHookup(
+        smartFurnitureHookup,
+      );
+
+    const { data } = await apiClient.post(
+      `${BASE_URL}/smart-furniture-hookups`,
+      {
+        id: result.id,
+        position: smartFurnitureHookup.position,
+        zoneID: smartFurnitureHookup.zone,
+      },
+    );
+    return data;
   },
 };
