4a4fbcd0c7fc11ceba4fe5c5726a2a93bb047da2
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-10 10:57:10 +0100

    feat: add SmartFurnitureHookupService with method to send an API request to save a new hookup
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/service/SmartFurnitureService.js
@@ -0,0 +1,18 @@
+import apiClient from "@/middlewares/authInterceptor";
+
+const BASE_URL = "/smart-furniture-hookup";
+
+export const SmartFurnitureHookupService = {
+  async saveSmartFurnitureHookup(smartFurnitureHookup) {
+    const { data } = await apiClient.post(
+      `${BASE_URL}/smart-furniture-hookups/`,
+      {
+        name: smartFurnitureHookup.name,
+        utilityType: smartFurnitureHookup.utilityType,
+        endpoint: smartFurnitureHookup.endpoint,
+      },
+    );
+
+    return data;
+  },
+};
