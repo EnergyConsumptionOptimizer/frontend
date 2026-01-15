21fd36b7b8f7091f908035676e25631223f7ca18
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-15 11:07:17 +0100

    feat: add find and update methods to the SmartFurnitureHookupService
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/service/SmartFurnitureService.js
+++ src/service/SmartFurnitureService.js
@@ -3,17 +3,24 @@ import apiClient from "@/middlewares/authInterceptor";
 const BASE_URL = "/smart-furniture-hookup";
 
 export const SmartFurnitureHookupService = {
-  async saveSmartFurnitureHookup(smartFurnitureHookup) {
-    const { data } = await apiClient.post(
-      `${BASE_URL}/smart-furniture-hookups/`,
-      {
-        name: smartFurnitureHookup.name,
-        utilityType: smartFurnitureHookup.utilityType,
-        endpoint: smartFurnitureHookup.endpoint,
-      },
+  async fetchSmartFurnitureHookups() {
+    return apiClient.get(`${BASE_URL}/smart-furniture-hookups/`);
+  },
+  async findSmartFurnitureHookup(id) {
+    return apiClient.get(`${BASE_URL}/smart-furniture-hookups/${id}`);
+  },
+  async addSmartFurnitureHookup(smartFurnitureHookup) {
+    return apiClient.post(`${BASE_URL}/smart-furniture-hookups/`, {
+      name: smartFurnitureHookup.name,
+      utilityType: smartFurnitureHookup.utilityType,
+      endpoint: smartFurnitureHookup.endpoint,
+    });
+  },
+  async updateSmartFurnitureHookup(id, payload) {
+    return apiClient.patch(
+      `${BASE_URL}/smart-furniture-hookups/${id}`,
+      payload,
     );
-
-    return data;
   },
   async deleteSmartFurnitureHookup(id) {
     return apiClient.delete(`${BASE_URL}/smart-furniture-hookups/${id}`);
