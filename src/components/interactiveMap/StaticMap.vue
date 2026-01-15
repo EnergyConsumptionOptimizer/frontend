3308b513a914edd13282e15ad01e6d976ab69810
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-15 11:08:52 +0100

    feat: add static map view for the dashboard
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/components/interactiveMap/StaticMap.vue
@@ -0,0 +1,35 @@
+<script setup>
+import SmartFurnitureHookup from "@/components/interactiveMap/SmartFurnitureHookup.vue";
+import Zone from "@/components/interactiveMap/Zone.vue";
+import InteractiveMapLayout from "@/layout/InteractiveMapLayout.vue";
+
+defineProps({
+  floorPlanSvg: { type: String },
+  zones: { type: Array, default: () => [] },
+  smartFurnitureHookups: { type: Array, default: () => [] },
+});
+</script>
+
+<template>
+  <interactive-map-layout :floor-plan-svg="floorPlanSvg">
+    <template #zones>
+      <zone
+        v-for="zone in zones"
+        :key="zone.id"
+        :editModeActive="false"
+        :zone="zone"
+      />
+    </template>
+
+    <template #hookups>
+      <smart-furniture-hookup
+        v-for="sfh in smartFurnitureHookups"
+        :key="sfh.id"
+        :editModeActive="false"
+        :smartFurnitureHookup="sfh"
+      />
+    </template>
+  </interactive-map-layout>
+</template>
+
+<style scoped></style>
