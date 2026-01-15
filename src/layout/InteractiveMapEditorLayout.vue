1be2f463b6dd8f367c4b23020b9958378572ab42
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-15 10:52:33 +0100

    style: edit InteractiveMapEditorLayout.vue mobile view
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/layout/InteractiveMapEditorLayout.vue
+++ src/layout/InteractiveMapEditorLayout.vue
@@ -2,11 +2,11 @@
 
 <template>
   <div
-    class="p-4 md:p-8 w-full lg:w-[1600px] flex flex-col justify-center items-center"
+    class="p-4 md:p-8 w-full xl:w-[1600px] flex flex-col justify-center items-center"
   >
-    <div class="grid grid-cols-1 lg:grid-cols-4 gap-6 flex-1 w-full">
+    <div class="grid grid-cols-1 xl:grid-cols-4 gap-6 flex-1 w-full">
       <!-- Main content area (floor plan, draw and edit buttons) -->
-      <div class="lg:col-span-3">
+      <div class="col-span-3">
         <div class="flex flex-col items-center space-y-4 w-full">
           <slot name="actions" />
           <slot name="floor-plan" />
@@ -14,7 +14,7 @@
       </div>
 
       <!-- Sidebar (Tree structure) -->
-      <div class="lg:col-span-1">
+      <div class="xl:col-span-1">
         <slot name="sidebar" />
       </div>
     </div>
