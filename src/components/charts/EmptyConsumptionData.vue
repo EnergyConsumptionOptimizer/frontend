0267d26a7b22e85d9e0c5f0ff02bf3b453838156
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-19 16:35:07 +0100

    feat: add empty data component for charts
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/components/charts/EmptyConsumptionData.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="absolute inset-0 flex flex-col items-center justify-center text-gray-400 select-none"
+  >
+    <span class="pi pi-chart-bar" style="font-size: 2rem"></span>
+    <h3 class="text-lg font-medium">No Data Available</h3>
+    <p class="text-sm">Try changing the selected filters or time range.</p>
+  </div>
+</template>
+
+<style scoped></style>
